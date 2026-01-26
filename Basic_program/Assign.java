c532cb03725ab87edcedacd557902127c04851ba
Pushpendra Dhakad <pushpendra.dhakad_cs25@gla.ac.in>
2026-01-26 21:48:01 +0530

    [Akash] Commit

---
+++ Basic_program/Assign.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Assign
+{
+	public static void main(String... args){
+      Scanner input = new Scanner(System.in);
+	  int a = input.nextInt();
+	  a+=10;
+	  System.out.println(a);
+	 }}			
